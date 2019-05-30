25187ffd915edfd3570d8ced2c0727abc7ada090
SATOSHI SUZUKI <31060964+szk2s@users.noreply.github.com>
2019-05-30 14:09:12 +0900

    Develop (#10)
    * Fix typo
    
    * Change URL of example code
    
    * Add executable bin/tdd-max
    
    * Change folder name- from `example` to `help`
    
    * Implement cli
    
    * Add boilerplate for cli
    
    * Change the format of cli command
    
    * - Update README
    
    * Increment npm version

--- bin/tdd-max.js
+++ bin/tdd-max.js
@@ -4,14 +4,11 @@ const fs = require('fs-extra');
 
 const opts = process.argv.slice(2);
 
-if (opts[0] === 'init') {
-  const dest = path.resolve(opts[1]);
+if (opts[0] === '--init' || opts[0] === 'init') {
+  const dest = path.resolve(opts[1] || './test');
   fs.copy(path.join(__dirname, '../boilerplate/test'), dest)
     .then(() =>
-      console.log(
-        'Success: test boilerplate has been generated into',
-        path.join(dest, 'test')
-      )
+      console.log('Success: test boilerplate has been generated into', dest)
     )
     .catch((err) => console.error(err));
 } else {
