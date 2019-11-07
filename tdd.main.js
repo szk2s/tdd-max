30b6e7bbdef466e5698fe05fcc1eb78ab38d36c9
Satoshi Suzuki <31060964+szk2s@users.noreply.github.com>
2019-11-07 18:52:09 +0900

    Develop (#21)
    * refactor(lib): more explicit DI
    
    * refactor(root): remove outdated file

--- tdd.main.js
+++ tdd.main.js
@@ -6,17 +6,23 @@ const maxAPI = require('max-api');
 const SuiteCollection = require('./lib/SuiteCollection');
 const Suite = require('./lib/Suite');
 const suiteCollection = new SuiteCollection();
-const requireTestFiles = require('./lib/requireTestFiles')(
+const { requireTestFiles } = require('./lib/processTestCode')(
   maxAPI,
   suiteCollection,
   Suite
 );
 const Test = require('./lib/Test');
 const BeforeEach = require('./lib/BeforeEach');
-require('./lib/global')(suiteCollection, maxAPI, Test, BeforeEach);
+const { defineGlobalVars } = require('./lib/global')(
+  suiteCollection,
+  maxAPI,
+  Test,
+  BeforeEach
+);
 
 const main = async () => {
   console.log('Starting');
+  defineGlobalVars();
   await requireTestFiles();
   await suiteCollection.run();
 };
