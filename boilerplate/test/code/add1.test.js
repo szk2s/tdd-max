29ba28bd8cfe405ac916f6f1414021f96578afb4
szk2s <suzukisjss@gmail.com>
2019-11-13 01:20:23 +0900

    feat: async function support

--- boilerplate/test/code/add1.test.js
+++ boilerplate/test/code/add1.test.js
@@ -1,16 +1,8 @@
-const assert = require('chai').assert;
-
-test(
-  'adds 1 to input number',
-  function*() {
-    maxAPI.outlet({ add1: 3 });
-    assert.equal(yield, 4, 'ordinary int');
-    maxAPI.outlet({ add1: 0.1 });
-    assert.equal(yield, 1.1, 'float val');
-    maxAPI.outlet({ add1: 100000 });
-    assert.equal(yield, 100001, 'big int');
-    maxAPI.outlet({ add1: -2.1 });
-    assert.equal(yield, -1.1, 'negative float');
-  },
-  'add1' // Name of the patcher which you want to test
-);
+const { expect } = require('chai');
+const TARGET = 'add1';
+test('adds 1 to input number', async () => {
+  expect(await fetch(TARGET, { add1: 3 })).to.equal(4);
+  expect(await fetch(TARGET, { add1: 0.1 })).to.equal(1.1);
+  expect(await fetch(TARGET, { add1: 100000 })).to.equal(100001);
+  expect(await fetch(TARGET, { add1: -2.1 })).to.equal(-1.1);
+});
--- src/MaxAPIFacade.ts
+++ src/MaxAPIFacade.ts
@@ -1,10 +1,10 @@
 import JSONValue = Max.JSONValue;
 
 export type fetch = (
-  req: JSONValue,
   handler: string,
-  reqName?: string,
-  timeLimit?: ms
+  req: JSONValue,
+  timeLimit?: ms,
+  reqName?: string
 ) => Promise<JSONValue>;
 
 export interface IMaxAPIFacade {
@@ -14,10 +14,10 @@ export interface IMaxAPIFacade {
 
 export class MaxAPIFacade implements IMaxAPIFacade {
   fetch(
-    req: JSONValue,
     handler: string,
-    reqName?: string,
-    timeLimit: ms = 1000
+    req: JSONValue,
+    timeLimit: ms = 1000,
+    reqName?: string
   ) {
     if (!reqName) {
       if (typeof req === 'string') reqName = req;
@@ -37,7 +37,7 @@ export class MaxAPIFacade implements IMaxAPIFacade {
     });
   }
   async patcherDir() {
-    const res = await this.fetch('get_path', 'path');
+    const res = await this.fetch('path', 'get_path');
     if (typeof res !== 'string') throw new Error('Invalid response');
     const removeHDDName = (str: string) => str.split(':')[1];
     return removeHDDName(res);
