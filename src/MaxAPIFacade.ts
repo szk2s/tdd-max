ff2228b29a0cb3ad0f62058a4c65baa2954a63b1
szk2s <suzukisjss@gmail.com>
2019-11-12 12:15:11 +0900

    refactor: extract method `request`

--- src/MaxAPIFacade.ts
+++ src/MaxAPIFacade.ts
@@ -1,17 +1,36 @@
+import JSONValue = Max.JSONValue;
+
 export class MaxAPIFacade {
   maxAPI: Max.API;
   constructor(maxAPI: Max.API) {
     this.maxAPI = maxAPI;
   }
-  patcherDir() {
-    return new Promise<string>((resolve, reject) => {
-      this.maxAPI.addHandler('path', (patcherPath: string) => {
-        resolve(patcherPath.split(':')[1]);
+  request(
+    req: JSONValue,
+    handler: string,
+    reqName?: string,
+    timeLimit: ms = 1000
+  ) {
+    if (!reqName) {
+      if (typeof req === 'string') reqName = req;
+      reqName = '';
+    }
+    return new Promise<JSONValue>((resolve, reject) => {
+      const timeout = setTimeout(() => {
+        console.log('timeout');
+        reject(new Error(`Session timeout: ${reqName} cannot get ${handler}`));
+      }, timeLimit);
+      this.maxAPI.addHandler(handler, (res: JSONValue) => {
+        clearTimeout(timeout);
+        resolve(res);
       });
-      this.maxAPI.outlet('get_path');
-      setTimeout(() => {
-        reject(new Error('Cannot get patcher path: session time out'));
-      }, 1000);
+      this.maxAPI.outlet(req);
     });
+  }
+  async patcherDir() {
+    const res = await this.request('get_path', 'path');
+    if (typeof res !== 'string') throw new Error('Invalid response');
+    const removeHDDName = (str: string) => str.split(':')[1];
+    return removeHDDName(res);
   }
 }
