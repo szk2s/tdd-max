72e11e30847192de69c64c5da0cfaa44d35d1cee
szk2s <suzukisjss@gmail.com>
2019-11-12 12:15:52 +0900

    refactor: remove unused property

--- src/BeforeEach.ts
+++ src/BeforeEach.ts
@@ -1,8 +1,6 @@
-import { BEFORE_EACH } from './constants/class-names';
 import { Test } from './Test';
 
 export class BeforeEach extends Test {
-  type: string = BEFORE_EACH;
   constructor(maxAPI: Max.API, { name, target, fn }: TddMax.testArgs) {
     super(maxAPI, { name, target, fn });
   }
--- src/constants/class-names.ts
+++
@@ -1,3 +0,0 @@
-export const SUITE = 'SUITE';
-export const TEST = 'TEST';
-export const BEFORE_EACH = 'BEFORE_EACH';
